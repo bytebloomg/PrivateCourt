e07dd8612b8226be3ebb3ba93f28e72766b752b5
hannahaidev <GearinYbarra35@hotmail.com>
2025-10-19 20:21:26 +0800

    use template

---
+++ app/src/App.tsx
@@ -0,0 +1,23 @@
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { config } from './config/wagmi';
+import { CourtApp } from './components/CourtApp';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider locale="en">
+          <CourtApp />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default App
